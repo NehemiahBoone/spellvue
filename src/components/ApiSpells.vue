d1c82e0daec79c583b9c254b4a0a037b1ef648a3
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-15 14:16:23 -0600

    setup

---
+++ src/components/ApiSpells.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'ApiSpells'
+  }
+</script>
+
+
+<style>
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+  import ApiSpells from "../components/ApiSpells.vue"
+  export default {
+    name: 'ApiSpells',
+    components: {
+      ApiSpells
+    }
+  }
+</script>
+
+
+<style>
+</style>
